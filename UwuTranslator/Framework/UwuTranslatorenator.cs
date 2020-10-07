29628b78eda2ed997f3707608931e695516edc1e
esda99 <esda98@gmail.com>
2020-10-07 17:15:08 -0400

    It works....what have i done.

---
+++ UwuTranslator/Framework/UwuTranslatorenator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace UwuTranslator.Framework
+{
+    public static class UwuTranslatorenator
+    {
+        //test string: Oh no, we made a mistake on the reports and now they don't work!
+        private static Dictionary<string, string> wordReplacements = new Dictionary<string, string>()
+        {
+            { "little", "wittle" },
+            { "Oh no", "Oopsie woopsie"},
+            { "we made a mistake", "we made a wittle fucko boingo"},
+            { "the", "da" }
+        };
+        private static Dictionary<string, string> letterReplacements = new Dictionary<string, string>
+        {
+            { "r", "w"}
+        };
+        
+        public static string translate(string input)
+        {
+            var translation = input;
+            //do a pass of words to entirely swap
+            foreach (var w in wordReplacements.Keys)
+            {
+                translation = translation.Replace(w, wordReplacements[w]);
+            }
+
+            //do a pass to swap individual letters
+            foreach (var l in letterReplacements.Keys)
+            {
+                translation = translation.Replace(l, letterReplacements[l]);
+            }
+
+            //return final string
+            return translation;
+        } 
+    }
+}
--- UwuTranslator/MainWindow.xaml.cs
+++ UwuTranslator/MainWindow.xaml.cs
@@ -12,17 +12,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UwuTranslator.Framework;
 
 namespace UwuTranslator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        public const string uwuPlaceHolder = "Your Uwu Output will be presented here!";
+        public const string inputPlaceholder = "Type your string to translate here!";
         public MainWindow()
         {
             InitializeComponent();
+            txtInput.Text = inputPlaceholder;
+            tblkUwuOutput.Text = uwuPlaceHolder;
+        }
+        
+        #region Input Placeholder
+        
+        private void TxtInput_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtInput.Text == inputPlaceholder)
+            {
+                txtInput.Text = "";
+            }
+        }
+
+        private void TxtInput_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtInput.Text == "")
+            {
+                txtInput.Text = inputPlaceholder;
+            }
+        }
+        
+        #endregion
+
+
+        private void TxtInput_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            //when no text is entered into the text box, show the placeholder in the label
+            if (txtInput is null || string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                tblkUwuOutput.Text = uwuPlaceHolder;
+                return;
+            }
+            tblkUwuOutput.Text = UwuTranslatorenator.translate(txtInput.Text);
         }
     }
 }
